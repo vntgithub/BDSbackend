3c38d39324f31e93a1dec24b2bfe75e1d54d4fc5
Vo Nhat Trieu <vntrieu25879@gmail.com>
2021-07-12 17:29:10 +0700

    modify cascade prodcut-image

--- src/main/java/vntrieu/train/bdsbackend/model/Product.java
+++ src/main/java/vntrieu/train/bdsbackend/model/Product.java
@@ -52,7 +52,7 @@ public class Product {
   @JoinColumn(name = "category_id")
   private Category category;
 
-  @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+  @OneToMany(mappedBy = "product")
   private Collection<Image> images;
 
   @ManyToOne
